40c0465069326384add35c0a827edc377ddb8cf5
Matheus Reis <reis.mmatheus@gmail.com>
2021-12-13 14:23:53 -0300

    Adding Facade

---
+++ Facade1/Facade1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Facade1.Facade;
+using Facade1.Subsistemas;
+using System;
+
+namespace Facade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria uma instância do Facade
+           MeuFacade concedeCreditoFacade = new MeuFacade();
+
+            // Cria uma instância de um  novo Cliente informando o nome
+            Cliente cliente1 = new Cliente("Macoratti");
+
+            //Utiliza o Facade para verificar condições de concessão ou não
+            bool resultado = concedeCreditoFacade.ConcederEmprestimo(cliente1, 199000.00);
+
+            //exibe o resultado
+            Console.WriteLine($"O empréstimo pleiteado pelo cliente {cliente1.Nome} foi " + (resultado ? "Aprovado" : "Negado"));
+
+            //aguarda
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Facade1/Facade1/Subsistemas/Cadastro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade1.Subsistemas
+{
+    public class Cadastro
+    {
+        public void CadastrarCliente(Cliente cliente)
+        {
+            Console.WriteLine($"Cadastro do cliente {cliente.Nome} concluído.");
+        }
+    }
+}
